0c644107a576906ee0d3a81cbc83ce3a330d201c
Arktetra <sameershrestha.sm@gmail.com>
2026-01-23 18:58:29 +0545

    add data structure for geometry, image and bin states

---
+++ src/curast/cuda/octree/data_structure.cu
@@ -0,0 +1,61 @@
+#include <cub/cub.cuh>
+#include "data_structure.h"
+#include "config.h"
+
+template<typename T>
+static void OctreeVoxelRasterizer::obtain(char*& chunk, T*& ptr, size_t count, size_t alignment) {
+    size_t offset = (reinterpret_cast<uintptr_t>(chunk) + alignment - 1) & ~(alignment - 1);
+    ptr = reinterpret_cast<T*>(offset);
+    chunk = reinterpret_cast<char*>(ptr + count);
+}
+
+template<typename T>
+size_t OctreeVoxelRasterizer::required_size(size_t N) {
+    char* size = nullptr;
+    return (reinterpret_cast<uintptr_t>(size) + MEM_ALIGNMENT - 1) & ~(MEM_ALIGNMENT - 1);
+}
+
+OctreeVoxelRasterizer::GeometryState OctreeVoxelRasterizer::GeometryState::from_chunk(char*& chunk, size_t N) {
+    GeometryState geom;
+    obtain(chunk, geom.depths, N, MEM_ALIGNMENT);
+    obtain(chunk, geom.morton_codes, N, MEM_ALIGNMENT);
+    obtain(chunk, geom.clamped, N * 3, MEM_ALIGNMENT);
+    obtain(chunk, geom.bboxes, N, MEM_ALIGNMENT);
+    obtain(chunk, geom.rgb, N * 3, MEM_ALIGNMENT);
+    obtain(chunk, geom.tiles_touched, N, MEM_ALIGNMENT);
+    cub::DeviceScan::InclusiveSum(nullptr, geom.scan_size, geom.tiles_touched, geom.tiles_touched, N);
+    obtain(chunk, geom.scanning_space, geom.scan_size, MEM_ALIGNMENT);
+    obtain(chunk, geom.point_offsets, N, MEM_ALIGNMENT);
+
+    return geom;
+}
+
+OctreeVoxelRasterizer::ImageState OctreeVoxelRasterizer::ImageState::from_chunk(char*& chunk, size_t N) {
+    ImageState img;
+    obtain(chunk, img.accum_alpha, N, MEM_ALIGNMENT);
+    obtain(chunk, img.wm_sum, N, MEM_ALIGNMENT);
+    obtain(chunk, img.n_contrib, N, MEM_ALIGNMENT);
+    obtain(chunk, img.ranges, N, MEM_ALIGNMENT);
+
+    return img;
+}
+
+OctreeVoxelRasterizer::BinningState OctreeVoxelRasterizer::BinningState::from_chunk(char*& chunk, size_t N) {
+    BinningState binning;
+    obtain(chunk, binning.point_list, N, MEM_ALIGNMENT);
+    obtain(chunk, binning.point_list_unsorted, N, MEM_ALIGNMENT);
+    obtain(chunk, binning.point_list_keys, N, MEM_ALIGNMENT);
+    obtain(chunk, binning.point_list_keys_unsorted, N, MEM_ALIGNMENT);
+    cub::DeviceRadixSort::SortPairs(
+        nullptr,
+        binning.sorting_size,
+        binning.point_list_keys_unsorted,
+        binning.point_list_keys,
+        binning.point_list_unsorted,
+        binning.point_list,
+        N
+    );
+    obtain(chunk, binning.list_sorting_space, N, MEM_ALIGNMENT);
+    
+    return binning;
+}
---
+++ src/curast/cuda/octree/data_structure.h
@@ -0,0 +1,66 @@
+#pragma once
+
+#include<cstdint>
+#include<cstdlib>
+#include<cuda_runtime_api.h>
+
+namespace OctreeVoxelRasterizer {
+
+    template<typename T>
+    static void obtain(char*& chunk, T*& ptr, size_t count, size_t alignment);
+
+    template<typename T>
+    size_t required_size(size_t N);
+
+    struct GeometryState {
+        size_t scan_size;
+        float* depths;
+        uint32_t* morton_codes;
+        char* scanning_space;
+        bool* clamped;
+        int4* bboxes;
+        float* rgb;
+        uint32_t* point_offsets;
+        uint32_t* tiles_touched;
+
+        /**
+         * Initiate `GeometryState` from a chunk.
+         * @param chunk     a pointer to the chunk.
+         * @param N         number of elements.
+         * @return a `GeometryState` object.
+         */
+        static GeometryState from_chunk(char*& chunk, size_t N);
+    };
+
+    struct ImageState {
+        uint2* ranges;
+        uint32_t* n_contrib;
+        float* accum_alpha;
+        float* wm_sum;
+
+        /**
+         * Initiate `ImageState` from a chunk.
+         * @param chunk     a pointer to the chunk.
+         * @param N         number of elements.
+         * @return an `ImageState` object.
+         */
+        static ImageState from_chunk(char*& chunk, size_t N);
+    };
+
+    struct BinningState {
+        size_t sorting_size;
+        uint64_t* point_list_keys_unsorted;
+        uint64_t* point_list_keys;
+        uint32_t* point_list_unsorted;
+        uint32_t* point_list;
+        char* list_sorting_space;
+
+        /**
+         * Initiate `BinningState` from a chunk.
+         * @param chunk     a pointer to the chunk.
+         * @param N         number of elements.
+         * @return a `BinningState` object.
+         */
+        static BinningState from_chunk(char*& chunk, size_t N);
+    };
+}
