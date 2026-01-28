2bd3de9e67e828cc7ccc903b832e5bc2dcfa2bfd
Arktetra <sameershrestha.sm@gmail.com>
2026-01-28 19:12:11 +0545

    add bindings

--- src/curast/cuda/ext.cpp
+++ src/curast/cuda/ext.cpp
@@ -1,5 +1,6 @@
 #include <torch/extension.h>
+#include "octree/api.h"
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
-    
+    m.def("rasterize_octree_voxels", &OctreeVoxelRasterizer::rasterize_cuda);
 }
